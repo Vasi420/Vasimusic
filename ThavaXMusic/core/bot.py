80ef5ab0fb379fd6d46608b2757874321964bb54
Vasi <157385207+Vasi420@users.noreply.github.com>
2024-06-05 10:35:29 +0530

    Update bot.py

--- ThavaXMusic/core/bot.py
+++ ThavaXMusic/core/bot.py
@@ -10,7 +10,7 @@ class THAVA(Client):
     def __init__(self):
         LOGGER(__name__).info(f"Starting Bot...")
         super().__init__(
-            name="ThavaX",
+            name="vibemachine",
             api_id=config.API_ID,
             api_hash=config.API_HASH,
             bot_token=config.BOT_TOKEN,
